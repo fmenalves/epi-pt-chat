a457cebf69fba3d02ea2f98653a25e4dfddcd3fc
fmenalves <fmenalves@gmail.com>
2025-03-28 15:32:02 +0000

    Adicionando nova pasta

---
+++ ragastest/ragasscore.py
@@ -0,0 +1,236 @@
+
+
+import os
+import pandas as pd
+
+from ragas import SingleTurnSample, EvaluationDataset
+from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
+
+from ragas.metrics import (
+    LLMContextPrecisionWithReference,
+#    LLMContextPrecisionWithoutReference,
+#    NonLLMContextPrecisionWithReference,
+    LLMContextRecall,
+#    NonLLMContextRecall,
+#    FactualCorrectness,
+    ContextEntityRecall,
+    NoiseSensitivity,
+    ResponseRelevancy,
+    Faithfulness,
+)
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+#from langchain_ollama.llms import OllamaLLM
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
+
+from ragassupp import present_result
+from ragassupp import present_result_melhorado
+
+from dotenv import load_dotenv
+
+from datasets import Dataset
+import ast
+import re
+
+load_dotenv()
+
+#LLM_URL = os.getenv("LLM_URL")
+
+localModel = "llama3:8b"
+
+llm = LangchainLLMWrapper(Ollama(model=localModel))
+#llm = LangchainLLMWrapper(Ollama(model="llama3.1:70b",base_url=LLM_URL))
+
+#embeddings = LangchainEmbeddingsWrapper(OllamaEmbeddings(model="nomic-embed-text:latest"))
+
+
+
+#GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+#llm = LangchainLLMWrapper(ChatGoogleGenerativeAI(model="gemini-2.0-flash"))
+#embeddings = LangchainEmbeddingsWrapper(GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+
+
+
+#OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+
+#embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings())
+#llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4o"))
+
+
+#def clean_context(text):
+#    text_clean = text.replace('\r\n', ' ').strip()  # Remover quebras de linha
+#    text_clean= re.sub(r'\s+', ' ', text_clean)  # Remover múltiplos espaços
+#    return text_clean
+
+
+
+def create_csv(name, per_res):
+    """
+    Creates a CSV file containing questions, generated answers, 
+    contexts, and ground truth answers.
+   
+    """
+
+    questions = per_res["Pergunta"].tolist()
+
+    ground_truth = per_res["Resposta"].tolist()
+
+    data = {"question": [], "answer": [], "contexts": [], "ground_truth": ground_truth}
+
+#    questions_melhoradas=[]
+
+
+    for pergunta in questions:
+        
+        
+#        resposta = present_result(pergunta)
+        resposta = present_result_melhorado(pergunta)
+
+        answer = resposta["response"]
+        context = resposta["context"]
+
+#        context = [clean_context(text) for text in context]
+
+        data["question"].append(pergunta)
+
+        data["answer"].append(answer)
+
+        data["contexts"].append(context)
+
+#        questions_melhoradas.append(resposta["nquery"])
+    
+#    questions = questions_melhoradas
+    
+    dataset = Dataset.from_dict(data)
+
+    df = pd.DataFrame(dataset)
+
+    df.to_csv(name+".csv", index=False, encoding="utf-8")
+
+
+
+def eval_sample(Pergunta, ground_truth, metrics):
+    """
+    Evaluates the response generated for a given question
+    Retrieves the response and contexts
+    Evaluates all metrics for it
+    Returns a dictionary containing all metrics
+
+    """
+
+
+    resposta = present_result_melhorado(Pergunta)
+#    resposta = present_result(Pergunta)
+
+    sample = SingleTurnSample(
+    user_input=Pergunta,
+    reference=ground_truth,
+    retrieved_contexts=resposta["context"],
+    response = resposta["response"]
+    )
+
+    # Defining each metric that we wanna see
+    results = {}
+    # Iterating through the metrics dictionary
+    for metric_name, metric in metrics.items():
+
+        try:
+            results[metric_name] = metric.single_turn_score(sample)
+
+        except Exception as e:
+            results[metric_name] = f"Error: {e}"
+
+    return results
+
+
+
+def evaluate_row(row, metrics):
+    """
+    Creates a single turn sample for the row
+    Evaluates all metrics for it
+    Returns a dictionary containing all metrics
+
+    """
+
+    # Create a SingleTurnSample for every row
+    sample = SingleTurnSample(
+        user_input=row['question'],
+        reference=row['ground_truth'],
+        retrieved_contexts=row['contexts'],
+        response = row['answer']
+    )
+    
+    # Evaluate metrics for the sample
+    results = {}
+    for metric_name, metric in metrics.items():
+        try:
+            results[metric_name] = metric.single_turn_score(sample)
+        except Exception as e:
+            results[metric_name] = f"Error: {e}"
+    
+    return results
+
+# Function to evaluate the entire dataframe of testset
+
+def evaluate_dataframe(df, metrics):
+    """
+    Iterates through the df test set
+    For every row uses evaluate_row function to get result dictionary
+    Append each dictionary to a list
+    Uses list to create the result dataframe
+
+    """
+
+    df["contexts"] = df["contexts"].apply(ast.literal_eval)
+
+    results = []
+    for _, row in df.iterrows():
+        row_results = evaluate_row(row, metrics)
+        results.append(row_results)
+    
+    res = pd.DataFrame(results)
+    
+    medias = res.mean().round(3)
+    res.loc["Mean"] = medias
+    
+    return res
+
+
+metrics = {
+            "Context Precision With Reference": LLMContextPrecisionWithReference(llm=llm),
+#            "Context Precision Without Reference": LLMContextPrecisionWithoutReference(llm=llm),
+#            "Non LLM Context Precision With Reference": NonLLMContextPrecisionWithReference(),
+            "Context Recall": LLMContextRecall(llm=llm),
+#            "Non LLM Context Recall": NonLLMContextRecall(),
+            "Context Entities Recall": ContextEntityRecall(llm=llm),
+            "Noise Sensitivity": NoiseSensitivity(llm=llm),
+#            "Response Relevancy": ResponseRelevancy(llm=llm,embeddings=embeddings),
+            "Faithfulness": Faithfulness(llm=llm),
+        }
+
+ 
+
+
+# Exemplo
+
+#per_res = pd.read_csv("Perguntas e resposta.csv", delimiter=",")
+#result = eval_sample(per_res["Pergunta"][0], per_res["Resposta"][0],metrics = metrics)
+
+
+
+#create_csv("dataset",per_res[3:7])
+dataset = pd.read_csv("dataset.csv", delimiter=",")
+df_evaluation = evaluate_dataframe(dataset,metrics = metrics)
+
+
+#test3 = pd.read_csv("test3.csv", delimiter=",")
+
+#df_evaluation = evaluate_dataframe(test3,metrics = metrics) 
+
+
---
+++ ragastest/ragassupp.py
@@ -0,0 +1,340 @@
+import os
+from llama_index.llms.ollama import Ollama
+from llama_index.core import PromptTemplate
+from dotenv import load_dotenv
+import pandas as pd
+
+from qdrant_client.http.models import Filter, FieldCondition, MatchValue, MatchAny
+
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Settings
+from llama_index.embeddings.langchain import LangchainEmbedding
+from langchain_community.embeddings import HuggingFaceEmbeddings #from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import VectorStoreIndex
+import qdrant_client
+
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.postprocessor.cohere_rerank import CohereRerank
+from llama_index.core.query_engine import RetrieverQueryEngine
+
+
+
+#docker pull qdrant/qdrant
+#docker run -p 6333:6333 -p 6334:6334 -v %cd%/qdrant_storage:/qdrant/storage:z -d qdrant/qdrant
+
+load_dotenv()
+
+LLM_URL = os.getenv("LLM_URL")
+URI_BD = os.getenv("URI_BD")
+cohere_api_key = os.getenv("COHERE_API_KEY")
+index_name = os.getenv("INDEX_NAME")
+
+client = qdrant_client.QdrantClient(URI_BD)
+
+text_qa_template_str = (
+    "Context information is"
+    " below.\n---------------------\n{context_str}\n---------------------\nUsing"
+    " the context information and not prior knowledge, answer"
+    " the question: {query_str}\nIf the context isn't helpful, you can also"
+    " answer the question on your own."
+    "Respond always in the language portuguese from portugal and do not use any word that is specific from brazil."
+    "Cite the source of the new information if you use it.\n"
+    "Answer:\n"
+)
+text_qa_template = PromptTemplate(text_qa_template_str)
+
+refine_template_str = (
+    "The original question is as follows: {query_str}\nWe have provided an"
+    " existing answer: {existing_answer}\nWe have the opportunity to refine"
+    " the existing answer (only if needed) with some more context"
+    " below.\n------------\n{context_msg}\n------------\nUsing the"
+    " context information and not prior knowledge, update or repeat the existing answer.\n"
+    " Respond always in the language portuguese from portugal and do not use any word that is specific from brazil.\n"
+    "Cite the source of the new information if you use it.\n"
+    "If the context isn't useful, return the original answer.\n"
+    "Refined Answer: "
+)
+
+refine_template = PromptTemplate(refine_template_str)
+
+
+#if OPENAI_KEY is not None:
+#    llm = OpenAI(api_key=OPENAI_KEY, model="gpt-3.5-turbo")
+#else:
+    ## pass
+llm = Ollama(
+    model="llama3.1:70b",
+    base_url=LLM_URL,
+    temperature=0,
+    request_timeout=120,
+)
+
+
+#embed_model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+embed_model_name = "sentence-transformers/all-mpnet-base-v2"
+
+metadatasource = pd.read_csv("finaldbpt2.csv", delimiter=",")
+
+
+
+def generate_queries(query: str):
+    query_gen_str = """You are a helpful assistant that can provide information about drugs. Please comercial drug names and/or substances, separated by a comma. You will return only the names separeted by comma and nothing more. The text to analyse is {query}."""
+    query_gen_prompt1 = PromptTemplate(query_gen_str)
+    query_gen_str2 = """You are a helpful assistant that can provide information about drugs. Please provide information additional about the drugs detected in the query. Important things to mention are active principles, side effects, drug class and interactions with other drugs for each detected drug.
+    Query: {query}
+    """
+    query_gen_prompt2 = PromptTemplate(query_gen_str2)
+#if OPENAI_KEY is not None:
+#    llm = OpenAI(api_key=OPENAI_KEY, model="gpt-3.5-turbo")
+#else:
+    ## pass
+
+    products = llm.predict(query_gen_prompt1, query=query)
+    # assume LLM proper put each query on a newline
+
+    add_info = llm.predict(query_gen_prompt2, query=query)
+
+    return products.replace(".", ""), add_info
+
+
+
+
+
+def create_filters(products, metadatasource):
+
+    metadatasource = pd.read_csv("finaldbpt2.csv", delimiter=",")
+
+    f = {"Nome_Comercial": [], "Substancia": []}
+    for word in products.split(","):
+        # print(word)
+        word = word.strip()
+        if any(
+            word.lower() == item.lower() for item in metadatasource["Nome Comercial"]
+        ):
+            # print("found")
+            val = metadatasource[
+                [
+                    word.lower() == item.lower()
+                    for item in metadatasource["Nome Comercial"]
+                ]
+            ]["Nome Comercial"].values
+            print(word, "comer")
+            for v in val:
+                f["Nome_Comercial"].append(v)
+        #  f.append(MetadataFilter(key="Nome_Comercial", value=val, operator="=="))
+        elif any(word.lower() == str(item).lower() for item in metadatasource["Subs"]):
+            val = metadatasource[
+                [word.lower() == str(item).lower() for item in metadatasource["Subs"]]
+            ]["Subs"].values
+
+            for v in val:
+                # f[v] = "Substancia"
+                f["Substancia"].append(v)
+
+        if metadatasource["Nome Comercial"].str.contains(word, case=False).any():
+            # print("found")
+            val = metadatasource[
+                metadatasource["Nome Comercial"].str.contains(word, case=False)
+            ]["Nome Comercial"].values
+            print(word, "comer")
+            for v in val:
+                # f[v] = "Nome_Comercial"
+                f["Nome_Comercial"].append(v)
+
+        #  f.append(MetadataFilter(key="Nome_Comercial", value=val, operator="=="))
+        elif (
+            metadatasource[metadatasource["Subs"].notna()]["Subs"]
+            .str.contains(word, case=False)
+            .any()
+        ):
+            val = metadatasource[
+                metadatasource["Subs"].notna()
+                & metadatasource["Subs"].str.contains(word, case=False)
+            ]["Subs"].values
+            for v in val:
+                # f[v] = "Substancia"
+                f["Substancia"].append(v)
+
+    return f
+
+
+
+
+
+
+
+
+def get_filters_qdrant(metadatasource, products):
+    filters = []
+    f = create_filters(products=products, metadatasource=metadatasource)
+
+    if len(f["Nome_Comercial"]) > 0:
+        filters.append(
+            FieldCondition(
+                key="Nome_Comercial",
+                match=MatchAny(any=list(set(f["Nome_Comercial"]))),
+            )
+        )
+    if len(f["Substancia"]) > 0:
+        filters.append(
+            FieldCondition(
+                key="Substancia",
+                match=MatchAny(any=list(set(f["Nome_Comercial"]))),
+            )
+        )
+        # return f
+    if len(filters) == 0:
+        return Filter(should=[])
+    if len(filters) > 0:
+        return Filter(should=filters)
+
+
+
+
+def retrieve_index(client, llm, index_name):
+    text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=10)
+    Settings.text_splitter = text_splitter
+
+    ## embed_model = OpenAIEmbedding(embed_batch_size=10)
+    embed_model = HuggingFaceEmbeddings(model_name=embed_model_name)
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+    ## Settings.num_output = 512
+    ## Settings.context_window = 3900
+    Settings.chunk_size = 1024
+    Settings.chunk_overlap = 64
+
+    vector_store = QdrantVectorStore(client=client, collection_name=index_name)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store,
+        text_qa_template=text_qa_template,
+        refine_template=refine_template,
+        transformations=[text_splitter],
+    )
+
+    return index
+
+
+
+
+
+def build_rag_pipeline(products, metadatasource):
+        
+    print("Building index...")
+    index = retrieve_index(client, llm, index_name)
+    print("Constructing query engine...")
+    filters_qdrant = get_filters_qdrant(
+            products=products, metadatasource=metadatasource
+        )
+    print(filters_qdrant)
+    #    app.logger.info("Filtros: {}".format(print(filters_qdrant)))
+
+    retriever = VectorIndexRetriever(
+        vector_store_kwargs={"qdrant_filters": filters_qdrant},
+        index=index,
+        # filters=filters,
+        similarity_top_k=30,
+    )
+    # configure response synthesizer
+    # response_synthesizer = get_response_synthesizer()
+    # reranker = SentenceTransformerRerank(
+    #    model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=15
+    # )
+
+    cohere_api_key = os.getenv("COHERE_API_KEY")
+
+    reranker = CohereRerank(api_key=cohere_api_key, top_n=15)
+        # assemble query engine
+    query_engine = RetrieverQueryEngine(
+        retriever=retriever,  # response_mode="compact",
+        node_postprocessors=[
+            reranker
+        ],  # ,SimilarityPostprocessor(similarity_cutoff=0.7)],
+    )
+
+    query_engine.update_prompts(
+        {
+            "response_synthesizer:text_qa_template": text_qa_template,
+            "response_synthesizer:refine_template": refine_template,
+        }
+    )
+
+    return query_engine
+
+
+
+
+
+def present_result_melhorado(query):
+#    start = timeit.default_timer()
+
+    products, add_info = generate_queries(query)
+
+    print("Detected products:", products)
+    rag_chain = build_rag_pipeline(products=products, metadatasource=metadatasource)
+    nquery = (
+        query
+        + "\n---------\nContext and more information about the products:\n"
+        + add_info
+    )
+
+    print("Pergunta melhorada: {}".format(nquery))
+
+#    app.logger.info("Pergunta melhorada: {}".format(nquery))
+
+    answer = rag_chain.query(nquery)
+#    end = timeit.default_timer()
+
+    node_context = answer.source_nodes
+
+    contexts = []
+
+    for i in range(len(node_context)):
+
+
+        contexts.append(node_context[i].text)
+
+    return {
+        "response": answer.response,
+        "context": contexts,
+        "nquery" : nquery
+#        "time": str(round(end - start)) + "s",
+    }
+
+def present_result(query):
+#    start = timeit.default_timer()
+
+    products, add_info = generate_queries(query)
+
+    print("Detected products:", products)
+    rag_chain = build_rag_pipeline(products=products, metadatasource=metadatasource)
+#    nquery = (
+#        query
+#        + "\n---------\nContext and more information about the products:\n"
+#        + add_info
+#    )
+
+    #print("Pergunta melhorada: {}".format(nquery))
+
+#    app.logger.info("Pergunta melhorada: {}".format(nquery))
+
+    answer = rag_chain.query(query)
+#    end = timeit.default_timer()
+
+    node_context = answer.source_nodes
+
+    contexts = []
+
+    for i in range(len(node_context)):
+
+
+        contexts.append(node_context[i].text)
+
+    return {
+        "response": answer.response,
+        "context": contexts
+#        "time": str(round(end - start)) + "s",
+    }
+
+
